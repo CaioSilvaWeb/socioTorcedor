ca4994a5cb54df02e484cd9952fe4d590a94d4e6
Caio Silva <56590620+CaioSilvaWeb@users.noreply.github.com>
2022-06-09 16:29:51 -0300

    Atualizaçao bd

--- src/database/bd.js
+++ src/database/bd.js
@@ -1,12 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env.DB_BASE,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        port: 5432,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres'
     }
 );
